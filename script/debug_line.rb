6f6f7da9c3d01588cbd39c6e60210882f044541d
k.harada <kaihatsu+k.harada@arts-net.co.jp>
2026-03-18 13:17:24 +0900

    Test LINE v1 push message

--- script/debug_line.rb
+++ script/debug_line.rb
@@ -1,3 +1,11 @@
 require "line/bot"
-consts = Line::Bot::V2::MessagingApi.constants.map(&:to_s)
-puts consts.grep(/[Cc]lient|[Aa]pi/).inspect
+puts Line::Bot::Client.inspect
+
+creds = Rails.application.credentials.line!
+client = Line::Bot::Client.new do |config|
+  config.channel_secret = creds[:channel_secret]
+  config.channel_token  = creds[:channel_token]
+end
+
+response = client.push_message(creds[:user_id], [{ type: "text", text: "Picroテスト通知 動作確認OK!" }])
+puts "HTTP status: #{response.code}"
